1db189c80dccd129994bbc9f3748807ae5cae665
Lucas Connors <lucas.revolutiontech@gmail.com>
2016-01-13 23:16:37 -0800

    Added id, email, and full name to UserProfile changelist in admin

--- flamingo/users/admin.py
+++ flamingo/users/admin.py
@@ -9,4 +9,15 @@ from django.contrib import admin
 from users.models import UserProfile
 
 
-admin.site.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'username', 'email', 'full_name',)
+
+    def username(self, userprofile):
+        return userprofile.user.username
+
+    def email(self, userprofile):
+        return userprofile.user.email
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
