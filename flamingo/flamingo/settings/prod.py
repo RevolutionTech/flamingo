074558ac55437a34a58d13b66ebc98ec45ebe9d1
Lucas Connors <lucas.revolutiontech@gmail.com>
2016-02-14 22:41:01 -0800

    Removed release from RAVEN_CONFIG, not compatible with Heroku

--- flamingo/flamingo/settings/prod.py
+++ flamingo/flamingo/settings/prod.py
@@ -19,5 +19,4 @@ class ProdSettings(BaseSettings):
                 secret_key=os.environ['FLAMINGO_RAVEN_SECRET_KEY'],
                 project_id='64150',
             ),
-            'release': raven.fetch_git_sha(self.TOP_DIR),
         }
