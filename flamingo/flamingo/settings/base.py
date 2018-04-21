69e32dca9c77d6b3d06e544d2608aac12ed5414e
Lucas Connors <lucas.revolutiontech@gmail.com>
2018-04-21 12:26:52 -0700

    Remove obsolete SessionAuthenticationMiddleware
    which serves no purpose in Django 1.10+

--- flamingo/flamingo/settings/base.py
+++ flamingo/flamingo/settings/base.py
@@ -40,7 +40,6 @@ class BaseSettings(DjangoDefaults):
         'django.middleware.common.CommonMiddleware',
         'django.middleware.csrf.CsrfViewMiddleware',
         'django.contrib.auth.middleware.AuthenticationMiddleware',
-        'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
         'django.contrib.messages.middleware.MessageMiddleware',
         'django.middleware.clickjacking.XFrameOptionsMiddleware',
         'django.middleware.security.SecurityMiddleware',
