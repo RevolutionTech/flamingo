799b14a80d99bbd990d0bb402da607602ac2b11e
Lucas Connors <lucas.revolutiontech@gmail.com>
2019-08-12 23:39:16 -0700

    Use .env file to store credentials and environment configuration (#82)

--- flamingo/manage.py
+++ flamingo/manage.py
@@ -3,9 +3,12 @@ import os
 import sys
 
 import cbsettings
+from dotenv import load_dotenv
 
 
 if __name__ == "__main__":
+    load_dotenv()
+
     os.environ.setdefault("DJANGO_SETTINGS_FACTORY", "flamingo.settings")
     cbsettings.configure('flamingo.settings.switcher')
 
