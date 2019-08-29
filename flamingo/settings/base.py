68bb622ad7ef133e816eb807ca9cd35e774e013b
Lucas Connors <lucas.revolutiontech@gmail.com>
2019-08-28 23:07:31 -0700

    Move project directory up a level

--- flamingo/settings/base.py
+++ flamingo/settings/base.py
@@ -20,7 +20,6 @@ def aws_s3_bucket_url(settings_class, bucket_name_settings):
 class BaseSettings(DjangoDefaults):
 
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-    TOP_DIR = os.path.dirname(BASE_DIR)
     SECRET_KEY = os.environ['FLAMINGO_SECRET_KEY']
 
     DEBUG = True
@@ -56,7 +55,7 @@ class BaseSettings(DjangoDefaults):
     TEMPLATES = [
         {
             'BACKEND': 'django.template.backends.django.DjangoTemplates',
-            'DIRS': [TOP_DIR + '/templates'],
+            'DIRS': [BASE_DIR + '/templates'],
             'APP_DIRS': True,
             'OPTIONS': {
                 'context_processors': [
@@ -91,11 +90,11 @@ class BaseSettings(DjangoDefaults):
     USE_TZ = True
 
     # Static files (CSS, JavaScript, Images) and Media
-    MEDIA_ROOT = os.path.join(TOP_DIR, 'media')
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-    STATIC_ROOT = os.path.join(TOP_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_DIRS = (
-        os.path.join(TOP_DIR, 'static'),
+        os.path.join(BASE_DIR, 'static'),
     )
     AWS_S3_KEY_PREFIX = 'media'
     AWS_S3_KEY_PREFIX_STATIC = 'static'
